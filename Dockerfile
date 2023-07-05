15cec78445e9ac18349ae746e3ef6ee11705a375
mjacquot <marcel.m.jacquot@gmail.com>
2023-07-05 15:14:51 -0700

    Modify Python dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM debian:latest
+# FROM debian:latest
 
-RUN apt-get update --fix-missing 
+# RUN apt-get update --fix-missing 
 
 # RUN apt install build-essential \
 #     zlib1g-dev \
@@ -14,15 +14,31 @@ RUN apt-get update --fix-missing
 #     wget libbz2-dev\
 #     -y
 
-RUN apt install python3 -y
+# RUN apt install python3 -y
+
+# RUN apt install python3-pip -y
 
 # WORKDIR /usr/src/app
 
 # COPY requirements.txt ./
-# RUN pip install --no-cache-dir -r requirements.txt
+
+
+# RUN pip3 install --no-cache-dir -r requirements.txt
 
 # COPY . .
 
 # WORKDIR "./my_site"
 
 # CMD [ "python", "manage.py", "runserver" ]
+
+FROM python:latest
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+
+RUN pip3 install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+CMD ["python3", "my_site/manage.py", "runserver", "0.0.0.0:8000"]
--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os #Need to add this after
+import os  # Need to add this after
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-6rvs55nos4o3g2ajmi438-v=miv@w%cyd@ew&owz+oky7ie4f#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
-# ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -57,9 +57,10 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'), # Add the folder called 'templates' to inherit from
+            # Add the folder called 'templates' to inherit from
+            os.path.join(BASE_DIR, 'templates'),
         ],
-        'APP_DIRS': True, # 'True' tells it to check applications folders for templates
+        'APP_DIRS': True,  # 'True' tells it to check applications folders for templates
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
