f3d7f520b2cbd318c7446324d3dd54a4322991d7
mjacquot <marcel.m.jacquot@gmail.com>
2023-07-10 23:52:08 -0700

    finish with car site project

--- my_car_site/cars/models.py
+++ my_car_site/cars/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Car(models.Model):
+    brand = models.CharField(max_length=30)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return f"Car is a {self.year} {self.brand}"
