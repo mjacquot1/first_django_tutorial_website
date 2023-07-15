579fabeadae0e362cb82322a4131e763fbb166f2
mjacquot <marcel.m.jacquot@gmail.com>
2023-07-15 15:54:11 -0700

    7.15

--- library/library/urls.py
+++ library/library/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.views.generic import RedirectView
 
 # www.example.com ---> example.com/app
 urlpatterns = [
-    path('catalog/',include('catalog.urls')),
+    path('catalog/', include('catalog.urls')),
     path('admin/', admin.site.urls),
-    path('',RedirectView.as_view(url='catalog/'))
+    path('', RedirectView.as_view(url='catalog/')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
